f2ed4546863e00ca36b4257eb73c8a5d6b190a1b
lourenca cabral silva <cabralsilvalourenca@gmail.com>
2021-07-02 14:34:48 +0200

    check validity input ok

--- script.js
+++ script.js
@@ -8,47 +8,42 @@ let tasksList = document.querySelector("#tasksList"); //récup ul
 // let liste = document.querySelector("#liste");
 let deleteButton; //recup delete button
 let storageContent;//valeur du local storage
+let error = document.getElementById("error");//recup message invalidité input
 
 /***
  * déclarations de fonctions
  **/ 
-// fonction controle champs input vide à résoudre
-// function checkSubmit(){
-//         if (newTask.value === ""){
-//             alert("Vous devez écrire quelque chose");
-//             return "false";
-//             }
-//         else{
-//             return "true";
-//         }
-//     }
 
 // fonction enregistrement dans le local storage
 function storage(){
-    window.localStorage.todoList = tasksList.innerHTML;
+    window.localStorage.todoList = tasksList.innerHTML;//intitulé + enregistrement de la liste dans le local storage
 };
 
 // fonction récupération des données du local storage
 function getValues(){
-    storageContent = window.localStorage.todoList;
+    storageContent = window.localStorage.todoList;//variable contenant la liste
     if(!storageContent){
         alert("Allons y, créons ensemble votre todoList !");
     }
     else {
-        tasksList.innerHTML = storageContent;
+        tasksList.innerHTML = storageContent;//récupération de la liste dans le html depuis le local storage
     }
 }
 
 /***
  * déroulement du code
  **/ 
-
 //ajouter une tâche
 addButton.addEventListener("click", function(e){//mise sur écoute de addButton
     e.preventDefault();
-    tasksList.innerHTML +=`<li class="liste">${newTask.value}<span><button id="deleteButton">supprimer</button></span></li>`;// création contenu de chaque li
-    newTask.value ="";//reinitialisation du champs de l'input
-    storage();//sauvegarde dans local storage de la li ainsi créée 
+    if (newTask.validity.valid){//vérification de validité de l'input
+        tasksList.innerHTML +=`<li class="liste">${newTask.value}<span><button id="deleteButton">supprimer</button></span></li>`;// création contenu de chaque li
+        storage();//sauvegarde dans local storage de la li ainsi créée 
+        newTask.value ="";//reinitialisation du champs de l'input
+    }
+    else{
+        error.innerHTML = "vous devez saisir une tâche de plus de deux caractères!";//message si input invalid
+    }  
 });
 
 
@@ -59,4 +54,4 @@ addButton.addEventListener("click", function(e){//mise sur écoute de addButton
 //     storage();
 // });
 
-getValues();
+getValues();// recuperation de la liste pré existante depuis le local storage
